e22f8b5015da90613d80d4779d86b43f6fe2125b
AmbroiseCollon <ambroise.collon@gmail.com>
2016-12-01 11:38:10 +0100

    First commit

--- Zozor+/ViewController.swift
+++ Zozor+/ViewController.swift
@@ -9,17 +9,125 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // MARK: - Properties
+    var stringNumbers: [String] = [String()]
+    var operators: [String] = ["+"]
+    var index = 0
+    var isExpressionCorrect: Bool {
+        if let stringNumber = stringNumbers.last {
+            if stringNumber.isEmpty {
+                if stringNumbers.count == 1 {
+                    let alertVC = UIAlertController(title: "Zéro!", message: "Démarrez un nouveau calcul !", preferredStyle: .alert)
+                    alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                    self.present(alertVC, animated: true, completion: nil)
+                } else {
+                    let alertVC = UIAlertController(title: "Zéro!", message: "Entrez une expression correcte !", preferredStyle: .alert)
+                    alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                    self.present(alertVC, animated: true, completion: nil)
+                }
+                return false
+            }
+        }
+        return true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var canAddOperator: Bool {
+        if let stringNumber = stringNumbers.last {
+            if stringNumber.isEmpty {
+                let alertVC = UIAlertController(title: "Zéro!", message: "Expression incorrecte !", preferredStyle: .alert)
+                alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                self.present(alertVC, animated: true, completion: nil)
+                return false
+            }
+        }
+        return true
     }
 
 
+    // MARK: - Outlets
+
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet var numberButtons: [UIButton]!
+
+    // MARK: - Action
+
+    @IBAction func tappedNumberButton(_ sender: UIButton) {
+        for (i, numberButton) in numberButtons.enumerated() {
+            if sender == numberButton {
+                addNewNumber(i)
+            }
+        }
+    }
+
+    @IBAction func plus() {
+        if canAddOperator {
+        	operators.append("+")
+        	stringNumbers.append("")
+            updateDisplay()
+        }
+    }
+
+    @IBAction func minus() {
+        if canAddOperator {
+            operators.append("-")
+            stringNumbers.append("")
+            updateDisplay()
+        }
+    }
+
+    @IBAction func equal() {
+        calculateTotal()
+    }
+
+
+    // MARK: - Methods
+
+    func addNewNumber(_ newNumber: Int) {
+        if let stringNumber = stringNumbers.last {
+            var stringNumberMutable = stringNumber
+            stringNumberMutable += "\(newNumber)"
+            stringNumbers[stringNumbers.count-1] = stringNumberMutable
+        }
+        updateDisplay()
+    }
+
+    func calculateTotal() {
+        if !isExpressionCorrect {
+            return
+        }
+
+        var total = 0
+        for (i, stringNumber) in stringNumbers.enumerated() {
+            if let number = Int(stringNumber) {
+                if operators[i] == "+" {
+                    total += number
+                } else if operators[i] == "-" {
+                    total -= number
+                }
+            }
+        }
+
+        textView.text = textView.text + "=\(total)"
+
+        clear()
+    }
+
+    func updateDisplay() {
+        var text = ""
+        for (i, stringNumber) in stringNumbers.enumerated() {
+            // Add operator
+            if i > 0 {
+                text += operators[i]
+            }
+            // Add number
+            text += stringNumber
+        }
+        textView.text = text
+    }
+
+    func clear() {
+        stringNumbers = [String()]
+        operators = ["+"]
+        index = 0
+    }
 }
-
